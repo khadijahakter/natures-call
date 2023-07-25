eea17886eca2b067cf389c44734fd0626d4dabf4
Hfear <hannahbfearns@gmail.com>
2023-07-25 11:57:20 -0400

    tailwind

--- frontend/NaturesCall/src/App.jsx
+++ frontend/NaturesCall/src/App.jsx
@@ -8,6 +8,10 @@ function App() {
 
   return (
     <>
+
+<h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
