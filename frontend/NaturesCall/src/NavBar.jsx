503b81b22bb420a08a25bd273e50cb4ec488ae47
Hfear <hannahbfearns@gmail.com>
2023-07-27 15:27:24 -0400

    fixed node stuff

--- frontend/NaturesCall/src/NavBar.jsx
+++ frontend/NaturesCall/src/NavBar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
   <nav class=" bg-green-900">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <Link to = {"/"} class="flex items-center">
-              <h1 class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Blogging platform
+              <h1 class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Natures call 
                </h1>
               <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"></span>
           </Link>
