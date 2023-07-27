b314c6bed0d04c4fff58a83b7f27ac3010875b22
Hackerdude374 <robertle982@gmail.com>
2023-07-27 15:15:01 -0400

    generated models

---
+++ server-side/migrations/20230727191414-create-review.js
@@ -0,0 +1,103 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Reviews', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      profileId: {
+        type: Sequelize.INTEGER
+      },
+      bathroomId: {
+        type: Sequelize.INTEGER
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      photo: {
+        type: Sequelize.STRING
+      },
+      wheelchair: {
+        type: Sequelize.INTEGER
+      },
+      unisex: {
+        type: Sequelize.INTEGER
+      },
+      emergencyCord: {
+        type: Sequelize.INTEGER
+      },
+      emergencyButton: {
+        type: Sequelize.INTEGER
+      },
+      petFriendly: {
+        type: Sequelize.INTEGER
+      },
+      requiresKey: {
+        type: Sequelize.INTEGER
+      },
+      handDryer: {
+        type: Sequelize.INTEGER
+      },
+      feminineProducts: {
+        type: Sequelize.INTEGER
+      },
+      toiletCovers: {
+        type: Sequelize.INTEGER
+      },
+      bidet: {
+        type: Sequelize.INTEGER
+      },
+      singleStall: {
+        type: Sequelize.INTEGER
+      },
+      multipleStall: {
+        type: Sequelize.INTEGER
+      },
+      changingTable: {
+        type: Sequelize.INTEGER
+      },
+      trashCan: {
+        type: Sequelize.INTEGER
+      },
+      goodFlooring: {
+        type: Sequelize.INTEGER
+      },
+      airFreshener: {
+        type: Sequelize.INTEGER
+      },
+      automatic: {
+        type: Sequelize.INTEGER
+      },
+      coatHook: {
+        type: Sequelize.INTEGER
+      },
+      brailleSign: {
+        type: Sequelize.INTEGER
+      },
+      hotWater: {
+        type: Sequelize.INTEGER
+      },
+      firstAid: {
+        type: Sequelize.INTEGER
+      },
+      sharpsDisposal: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Reviews');
+  }
+};
