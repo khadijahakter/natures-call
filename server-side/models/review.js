41ddd15f32cd181aa1450f134a6b65c05be434ad
Khadijah Akter <khadijahakter123@gmail.com>
2023-07-27 15:50:46 -0400

    added tables

--- server-side/models/review.js
+++ server-side/models/review.js
@@ -4,13 +4,10 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Review extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      this.belongsTo(models.Bathroom);
+      this.belongsTo(models.User);
+    
     }
   }
   Review.init({
