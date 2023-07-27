6f3edb8c62625e6de33d9d35ebd14aea924692cd
Hfear <hannahbfearns@gmail.com>
2023-07-27 15:18:12 -0400

    basic route

--- frontend/NaturesCall/src/main.jsx
+++ frontend/NaturesCall/src/main.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
+import Navbar from './NavBar.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navbar/>,
+    children : [
+      // {
+      //   path: "/login",
+      //   element: <Login/>,
+      // },
+      // {
+      //   path: "/signup",
+      //   element: <Signup/>,
+      // },
+      
+    ],
+    
+  },
+])
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
