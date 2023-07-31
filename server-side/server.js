acc5f0f84cf29c3a2192c4651259ffe1d6e40bbb
Hfear <hannahbfearns@gmail.com>
2023-07-30 21:10:39 -0400

    post a bathroom works

--- server-side/server.js
+++ server-side/server.js
@@ -156,6 +156,56 @@ app.get("/bathrooms/:bathroomId", async (req, res) => {
   }
 });
 
+  //create a bathroom
+  app.post("/bathrooms", async (req, res) => {
+    try{
+
+      const newbathroom = await Bathroom.create({
+        sourceid: req.body.sourceid,
+        address: req.body.address,
+        lat: req.body.lat,
+        lng: req.body.lng,
+        name: req.body.name,
+        rating: req.body.rating,
+        content: req.body.content,
+        photo: req.body.photo,
+        wheelchair: req.body.wheelchair,
+        unisex: req.body.unisex,
+        emergencyCord: req.body.emergencyCord,
+        emergencyButton: req.body.emergencyButton,
+        petFriendly: req.body.petFriendly,
+        requiresKey: req.body.requiresKey,
+        handDryer: req.body.handDryer,
+        feminineProducts: req.body.feminineProducts,
+        toiletCovers: req.body.toiletCovers,
+        bidet: req.body.bidet,
+        singleStall: req.body.singleStall,
+        multipleStall: req.body.multipleStall,
+        changingTable: req.body.changingTable,
+        trashCan: req.body.trashCan,
+        goodFlooring: req.body.goodFlooring,
+        airFreshener: req.body.airFreshener,
+        automatic: req.body.automatic,
+        coatHook: req.body.coatHook,
+        brailleSign: req.body.brailleSign,
+        hotWater: req.body.hotWater,
+        firstAid: req.body.firstAid,
+        sharpsDisposal: req.body.sharpsDisposal,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        UserId: req.session.userId,
+      });
+      
+
+
+      res.status(201).json(newbathroom);
+    }
+    catch(err){
+      console.error(err);
+      res.status(500).send({message: err.message});
+    }
+  });
+
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
