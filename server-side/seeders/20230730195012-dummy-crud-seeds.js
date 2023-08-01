30ff87a7ca12e5120546545af26c0723e36a2e60
Hfear <hannahbfearns@gmail.com>
2023-07-31 20:02:38 -0400

    added more data to the seeders

--- server-side/seeders/20230730195012-dummy-crud-seeds.js
+++ server-side/seeders/20230730195012-dummy-crud-seeds.js
@@ -3,6 +3,28 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     // Seed data for bathrooms table
+
+     // Seed data for users table
+     await queryInterface.bulkInsert('users', [
+      {
+        name: 'John Doe',
+        email: 'john@example.com',
+        password: 'hashed_password_here',
+        photo: 'https://example.com/user.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Jane Smith',
+        email: 'jane@example.com',
+        password: 'hashed_password_here',
+        photo: 'https://example.com/jane.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      // Add more user entries here if needed
+    ]);
+
     await queryInterface.bulkInsert('bathrooms', [
       {
         sourceid: 'bathroom1',
@@ -11,6 +33,18 @@ module.exports = {
         lng: '-74.654321',
         name: 'Main Street Bathroom',
         rating: 4,
+        UserId: 1, // Foreign key reference to the user
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        sourceid: 'bathroom2',
+        address: '456 Elm St',
+        lat: '40.987654',
+        lng: '-74.123456',
+        name: 'Elm Street Restroom',
+        rating: 3,
+        UserId: 2, // Foreign key reference to the user
         createdAt: new Date(),
         updatedAt: new Date(),
       },
@@ -44,24 +78,45 @@ module.exports = {
         hotWater: 1,
         firstAid: 1,
         sharpsDisposal: 0,
+        UserId: 2, // Foreign key reference to the user
+        bathroomId: 2, // Foreign key reference to the bathroom
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        content: 'Clean and well-maintained restroom.',
+        photo: 'https://example.com/bathroom2.jpg',
+        wheelchair: 1,
+        unisex: 0,
+        emergencyCord: 1,
+        emergencyButton: 0,
+        petFriendly: 0,
+        requiresKey: 1,
+        handDryer: 0,
+        feminineProducts: 1,
+        toiletCovers: 1,
+        bidet: 1,
+        singleStall: 0,
+        multipleStall: 1,
+        changingTable: 0,
+        trashCan: 1,
+        goodFlooring: 1,
+        airFreshener: 0,
+        automatic: 1,
+        coatHook: 1,
+        brailleSign: 0,
+        hotWater: 1,
+        firstAid: 0,
+        sharpsDisposal: 1,
+        UserId: 1, // Foreign key reference to the user
+        bathroomId: 2, // Foreign key reference to the bathroom
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       // Add more review entries here if needed
     ]);
 
-    // Seed data for users table
-    await queryInterface.bulkInsert('users', [
-      {
-        name: 'John Doe',
-        email: 'john@example.com',
-        password: 'hashed_password_here',
-        photo: 'https://example.com/user.jpg',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      // Add more user entries here if needed
-    ]);
+   
   },
 
   async down(queryInterface, Sequelize) {
