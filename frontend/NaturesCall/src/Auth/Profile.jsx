916cf41217f203ca7f87a83e901ff6688410833a
Hackerdude374 <robertle982@gmail.com>
2023-08-03 11:26:38 -0400

    on profile displays bathrooms and reviews

--- frontend/NaturesCall/src/Auth/Profile.jsx
+++ frontend/NaturesCall/src/Auth/Profile.jsx
@@ -7,41 +7,36 @@ import './Profile.css';
 // loader for user bathrooms
 export async function loader({ params }) {
 
-  
-  const response = await fetch(`http://localhost:4000/bathrooms/user/${params.UserId}`);
-
+  const UserId = 5;
+  //const response = await fetch(`http://localhost:4000/bathrooms/user/${params.UserId}`);
+  const response = await fetch(`http://localhost:4000/bathrooms/user/5`);
   const UserBathrooms =await response.json();
-  return {UserBathrooms};
+  return UserBathrooms;
 
+}
+export async function ReviewLoader({ params }) {
+  //ask Chad on how t get userId 
+  //const response = await fetch(`http://localhost:4000/${params.UserId}/reviews`);
+  const response = await fetch(`http://localhost:4000/5/reviews`);
+  const UserReviews = await response.json();
+  return UserReviews ;
 }
 
 
 
 export default function Profile() {
-  const [userBathrooms, setUserBathrooms] = useState([]);
+  const userBathrooms = useLoaderData(loader); // Use the existing loader for bathrooms
+  const reviewsData = useLoaderData(ReviewLoader); // Use the new loader for bathroom reviews
 
-  useEffect(() => {
-    async function fetchUserBathrooms() {
-      try {
+
    
-       const userId = 5; // Replace 5 with the actual user's ID or fetch it from the logged-in user's data
-        const response = await axios.get(`http://localhost:4000/bathrooms/user/${userId}`);
-        const userBathroomsData = response.data;
-        
-
-        setUserBathrooms(userBathroomsData);
-      } catch (error) {
-        console.error("Error fetching user bathrooms data:", error);
-      }
-    }
-    fetchUserBathrooms();
-  }, []);
 
   return (
 
     <div>
 
     <h1 className="profile-header">Your Profile</h1>
+   { /*bathrooms*/}
     <div className="bathrooms-container">
        <h2 className = "bathroom-header"> Your Bathrooms </h2>
       {userBathrooms.map((bathroom) => (
@@ -55,6 +50,18 @@ export default function Profile() {
           <p>Content: {bathroom.content}</p>
           <img src={bathroom.photo} alt={`Photo of ${bathroom.name}`} />
           {/* Render other bathroom details here */}
+          <hr />
+        </div>
+      ))}
+    </div>
+        {/*reviews*/}
+        <div className="reviews-container">
+       <h2 className = "review-header"> Your Reviews </h2>
+      {reviewsData.map((review) => (
+        <div key={review.id} className="review-item">
+          <p>Review Content: {review.content}</p>
+        
+          {/* Render other review details here */}
           <hr />
         </div>
       ))}
